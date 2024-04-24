c14bf9813397d368bea33f067e3ae81892f14b18
ThembakaziNgamlana <thembakazingamlana25@gmail.com>
2024-04-24 16:22:30 +0200

    adding code for htlm and css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,3 +10,30 @@ import { Component } from '@angular/core';
 export class LoginComponent {
 
 }
+// public loginForm!: FormGroup
+
+//   constructor(private formbuilder: FormBuilder,private http: HttpClient, private router: Router) { }
+
+//   ngOnInit(): void {
+//     this.loginForm = this.formbuilder.group({
+//       email: [''],
+//       password: ['', Validators.required]
+//     })
+//   }
+//   login(){
+//     this.http.get<any>("http://localhost:3000/signupUsersList")
+//     .subscribe(res=>{
+//       const user = res.find((a:any)=>{
+//         return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password 
+//       });
+//       if(user){
+//         alert('Login Succesful');
+//         this.loginForm.reset()
+//       this.router.navigate(["home"])
+//       }else{
+//         alert("user not found")
+//       }
+//     },err=>{
+//       alert("Something went wrong")
+//     })
+//   }
