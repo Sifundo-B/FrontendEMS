6a00444cecde0feea502a00007225477dd84f731
Busisiwetshongozi <busisiwetshongozi@gmail.com>
2024-04-29 11:42:05 +0200

    Registrationaform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/my-service.service.ts
+++ src/app/services/my-service.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyServiceService {
 
-constructor() { }
+constructor(private http: HttpClient) { 
 
+this.http.get('http://localhost:3000/employeeData').subscribe();
+  
+    }
+  
+  
 }
