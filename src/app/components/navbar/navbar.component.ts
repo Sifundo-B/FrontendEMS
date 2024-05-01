d47f7d76bb870190b34b95699190e99a1fdf0c66
Gomolemo <tshabalalagomolemo@gmail.com>
2024-05-01 13:08:57 +0200

    Fixed SideBar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
     navTitle: string = 'Employee Management';
-    userName: string = 'admin';
-    userImg: string = 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg';    
+    userName: string = 'admin: David Brown';
+    userImg: string = 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/media_bank/202309/elon-musk-252648408-16x9.jpg?VersionId=9KYZpqpoY3WvH8eVZg54mmkpTGfvPCWj&size=690:388';    
+
+    
+
+    
 }
